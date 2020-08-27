b8561e4c6860ef4985811c8a67ac5671ea0d92c0
Paulina Lubasinska <lubasinska.paulina@gmail.com>
2020-08-27 12:05:01 +0200

    Updates

---
+++ precache-manifest.dc193d7568f6cea4ea48118e907716a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7826d322d27583b40e87d79028d8174d",
+    "url": "/calculator-reactjs/index.html"
+  },
+  {
+    "revision": "2b2bb85ff3029717d801",
+    "url": "/calculator-reactjs/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "2b2bb85ff3029717d801",
+    "url": "/calculator-reactjs/static/js/2.978031fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator-reactjs/static/js/2.978031fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf97c66ffcaef9cf48b8",
+    "url": "/calculator-reactjs/static/js/main.e1ce0d4f.chunk.js"
+  },
+  {
+    "revision": "10169c3312dbf4fe9ecf",
+    "url": "/calculator-reactjs/static/js/runtime-main.bd1c43b2.js"
+  }
+]);
